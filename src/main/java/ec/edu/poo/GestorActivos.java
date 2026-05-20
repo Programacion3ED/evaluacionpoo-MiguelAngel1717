60014472279df7b906ee5d7e5ec1c15ccdc5f031
Laboratorio <laboratorio@udla-ec.int>
2026-05-20 11:08:29 -0500

    Miguel Torres v2

--- src/main/java/ec/edu/poo/GestorActivos.java
+++ src/main/java/ec/edu/poo/GestorActivos.java
@@ -8,36 +8,59 @@ public class GestorActivos {
     private int contador;
 
     public GestorActivos() {
-        // TODO: inicializar arreglo y contador
+        activos = new ActivoDigital[MAX_ACTIVOS];
+        contador = 0;
     }
 
     public void reiniciar() {
-        // TODO: reiniciar arreglo y contador
+        activos = new ActivoDigital[MAX_ACTIVOS];
+        contador = 0;
     }
 
     public boolean registrarActivo(ActivoDigital activo) {
-        // TODO: registrar si hay espacio y si el código no existe
-        return false;
+        if (contador >= MAX_ACTIVOS) return false;
+        if (buscarPorCodigo(activo.getCodigo()) != null) return false;
+
+        activos[contador] = activo;
+        contador++;
+        return true;
     }
 
     public ActivoDigital buscarPorCodigo(String codigo) {
-        // TODO: búsqueda secuencial por código
+        for (int i = 0; i < contador; i++) {
+            if (activos[i].getCodigo().equals(codigo)) {
+                return activos[i];
+            }
+        }
         return null;
     }
 
     public int contarActivosCriticos() {
-        // TODO: contar activos con nivelRiesgo >= 8
-        return 0;
+        int criticos = 0;
+        for (int i = 0; i < contador; i++) {
+            if (activos[i].getNivelRiesgo() >= 8) {
+                criticos++;
+            }
+        }
+        return criticos;
     }
 
     public double calcularPromedioRiesgo() {
-        // TODO: calcular promedio de riesgo
-        return 0;
+        if (contador == 0) return 0;
+
+        int suma = 0;
+        for (int i = 0; i < contador; i++) {
+            suma += activos[i].getNivelRiesgo();
+        }
+        return (double) suma / contador;
     }
 
     public boolean aplicarParcheActivo(String codigo) {
-        // TODO: buscar activo y cambiar parcheAplicado a true
-        return false;
+        ActivoDigital activo = buscarPorCodigo(codigo);
+        if (activo == null) return false;
+
+        activo.setParcheAplicado(true);
+        return true;
     }
 
     public int obtenerCantidadActivos() {
